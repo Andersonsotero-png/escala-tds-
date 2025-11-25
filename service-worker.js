bdc233570ff82ca713a3b090798eab1d6f93f677
Parquinho tds. <anderson.sotero@aluno.unifametro.edu.br>
2025-11-25 13:16:11 -0300

    Implement caching in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,15 @@
-self.addEventListener("install", e => {
-    self.skipWaiting();
+const CACHE_NAME = 'parquinho-v1';
+const PRECACHE_URLS = ['/', 'index.html', 'style.css', 'script.js', 'icon-192.png', 'icon-512.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE_URLS)));
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", () => { });
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+});
